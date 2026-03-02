e0ba790a27b697830c4f42fed10bf50de147784e
Nandini390 <Shara.nandini123@gmail.com>
2026-03-02 14:29:26 +0530

    added modules

---
+++ src/main/java/org/example/uberprojectentityservice/Models/Booking.java
@@ -0,0 +1,33 @@
+package org.example.uberprojectentityservice.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking extends BaseModel{
+    @Enumerated(value= EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private Long totalDistance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="driver_id")
+    private Driver driver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="passenger_id")
+    private Passenger passenger;
+}
---
+++ src/main/java/org/example/uberprojectentityservice/Models/Driver.java
@@ -0,0 +1,32 @@
+package org.example.uberprojectentityservice.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","bookings"})
+public class Driver extends BaseModel{
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String licenseNumber;
+
+    //1:n, driver:booking
+    @OneToMany(mappedBy = "driver", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
+    private List<Booking> bookings= new ArrayList<>();
+}
